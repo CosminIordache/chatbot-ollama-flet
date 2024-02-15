738c3a40aa8bd264eeb65f757d527e6708cedefe
CosminIordache <cosminionutiordache@gmail.com>
2024-02-15 15:44:45 +0100

    text change

--- main.py
+++ main.py
@@ -23,19 +23,19 @@ def main(page: Page):
         if user_input:
 
             #Add the user message to the message list
-            chat_view.controls.append(Text(value=f"{user_input}", color="blue"))
+            chat_view.controls.append(Text(value=f"You: {user_input}"))
             #Set the TextField to empty when the user sends a message
             txt_input.value = ""
             #Update the page to show instant the message
             page.update()
             
             #Generate a pre response and add it to the message list to get all the message with "\n"
-            pre_response = Text(value="", color="green")
+            pre_response = Text(value="", color= "green")
             chat_view.controls.append(pre_response)
             page.update()
             
             #Create a empty string and concat the response that the ia generate and we can see the response in real time becouse we got the pre response
-            response_text = ""
+            response_text = "Bot: "
             for response_generate in generate(user_input, context):
                 response_text += response_generate + " "
                 pre_response.value = response_text
@@ -43,15 +43,15 @@ def main(page: Page):
                 page.update()
 
     #App structure
-    chat_view = ListView(expand=True, auto_scroll= True)
-    txt_input = TextField(hint_text="Type your message here", expand=True, autofocus=True, on_submit= send_message, multiline= True)
-    send_btn = IconButton(icon=icons.SEND, on_click= send_message)
-    input_row = Row([txt_input, send_btn], alignment=alignment.bottom_center)
+    chat_view = ListView(expand= True, auto_scroll= True)
+    txt_input = TextField(hint_text="Type your message here", expand= True, autofocus= True, on_submit= send_message, multiline= True)
+    send_btn = IconButton(icon= icons.SEND, on_click= send_message)
+    input_row = Row([txt_input, send_btn], alignment= alignment.bottom_center)
 
     #Title
     page.title = 'Chat IA Local'
     #Add all componenst to the page
-    page.add(Column([chat_view, input_row], expand=True))
+    page.add(Column([chat_view, input_row], expand= True))
 
 if __name__ == "__main__":
     app(target=main)
